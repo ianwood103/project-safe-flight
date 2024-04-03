a2540eb93dacfef48ff0d1e8729e115a8e8ba817
Ian Wood <ianwood103@gmail.com>
2024-04-03 01:54:12 -0400

    create customizable design

--- main.py
+++ main.py
@@ -1,13 +1,34 @@
-import osmnx as ox
+import os
+import imgkit
 
-# Configure the place, network type, and specify that you want to retain all
-place = 'Atlanta, Georgia, USA'
-network_type = 'drive'
-ox.config(use_cache=True, log_console=True)
+def parse_template(template, data):
+    for key, value in data.items():
+        template = template.replace(f'{{{{ {key} }}}}', value)
+    return template
 
-# Fetch OSM street network from the specified place
-graph = ox.graph_from_place(place, network_type=network_type)
+options = {
+    'format': 'png',
+    'crop-h': '322',
+    'crop-w': '208',
+    'crop-x': '8',
+    'crop-y': '8',
+	'enable-local-file-access': None,
+	'quality': '100'
+}
 
-# Plot the street network and save to disk
-fig, ax = ox.plot_graph(ox.project_graph(graph), show=False, close=True)
-fig.savefig('atlanta_streets.png')
+data = {
+    'collisions': '33',
+    'month': 'February'
+}
+
+with open('templates/cityscape/template.html', 'r') as file:
+    template = file.read()
+
+parsed_template = parse_template(template, data)
+    
+with open('templates/cityscape/parsed_template.html', 'w') as file:
+    file.write(parsed_template)
+
+if not os.path.exists("output"):
+    os.makedirs("output")
+imgkit.from_file('templates/cityscape/parsed_template.html', 'output/cityscape.png', options=options)
